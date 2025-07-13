d82bd45f8c12a480b8b54d26c9f93c0dd4f463ee
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-07-13 19:26:44 +0100

    updated route data

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -2,13 +2,13 @@ export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
   const config = {
-    accountAssociation: {
-      header:
-        "",
-      payload: "",
-      signature:
-        "",
-    },
+   
+  "accountAssociation": {
+    "header": "eyJmaWQiOjg3MjkzNSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEFEMDYzMjVEMjExODhEQ2FlMEJFRDMxODQ0ZDg3MjllQ0Y3MDQxRGIifQ",
+    "payload": "eyJkb21haW4iOiJ0cml2aWEtYmxpdHoudmVyY2VsLmFwcCJ9",
+    "signature": "MHg0ODlkYzQwNGE5ZGNjZGI4NjQyNDU4ODY2NTE4YjZmNDhjN2Q2OGY2MTYyZjc4NjZkYWNhYzMwOWYyMTNlMzMzMTljZGRjYmYwYWI5YTU3ZjBlMDY2Y2NhNDUwYmJjMWM5NGI0YjJlZmM3MGI2MWQxZDhiZTJhYjgzMmExZjI1MjFi"
+  },
+
     frame: {
       version: "1",
       name: "TriviaBlitz",
